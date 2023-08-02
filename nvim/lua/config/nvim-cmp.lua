9dac313f85cf334bc4dd819ed59db55302907988
fischerbach <rafalrybnik@yahoo.com>
2023-08-02 20:40:03 +0200

    update

--- nvim/lua/config/nvim-cmp.lua
+++ nvim/lua/config/nvim-cmp.lua
@@ -23,15 +23,15 @@ cmp.setup({
         ['<C-j>'] = cmp.mapping.select_next_item(),
         -- Use <CR>(Enter) to confirm selection
         -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
         -- A super tab
         -- sourc: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
         ["<Tab>"] = cmp.mapping(function(fallback)
             -- Hint: if the completion menu is visible select next one
-           -- if cmp.visible() then
-           --     cmp.select_next_item()
-            if has_words_before() then
+            if cmp.visible() then
+                cmp.select_next_item()
+	   elseif has_words_before() then
                 cmp.complete()
             else
                 fallback()
